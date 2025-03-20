2983b4fa07a773baf4d7708fa2be5971eab7478f
devarsh2007 <sunilpatel2131@gmail.com>
2025-03-20 08:55:17 +0530

    py

---
+++ decision_making_statements/ifelse1.py
@@ -0,0 +1,9 @@
+# check number is positive or negetive
+
+number = int(input("enter a number : "))
+
+if number<0:
+    print(f"{number} is negetive")
+    
+else:
+    print(f"{number} is positive")
---
+++ decision_making_statements/ifelse4.py
@@ -0,0 +1,10 @@
+# check a latter is vowel or consonent 
+
+# vowel -> a,e,i,o,u
+
+latter = input("enter a latter : ")
+
+if latter=="a" and latter=="e" and latter=="i" and latter=="o" and latter=="u":
+    print("vowel")
+    
+    
