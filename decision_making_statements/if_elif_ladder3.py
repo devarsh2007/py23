0496b4ee27f475f0a70b8ae4fa7c25d1285fd763
devarsh2007 <sunilpatel2131@gmail.com>
2025-03-22 09:04:10 +0530

    nested if

--- decision_making_statements/if_elif_ladder3.py
+++ decision_making_statements/if_elif_ladder3.py
@@ -13,10 +13,27 @@ science = int(input("enter science marks : "))
 eng = int(input("enter english marks : "))
 
 if (maths<0 or maths>100) or (eng<0 or eng>100) or (science<0 or science>100): 
-    print("wrong")
+    print("wrong marks!!!")
 
-# persantage
-marks = (maths+eng+science) / 3
-print(round(marks,2))
+else:
+    
+    # persantage
+    marks = (maths+eng+science) / 3
+    print(round(marks,2))
 
-marks = round(marks,2)
+    marks = round(marks,2)
+
+    if marks>=90:
+        print("Grad : A")
+        
+    elif marks>=80 and marks<=89 : 
+        print("Grad : B")
+        
+    elif marks>=70 and marks<=79:
+        print("Grad : C")
+        
+    elif marks>=60 and marks<=69:
+        print("Grad : D")
+
+    else:
+        print("Grad : F [FAIL]")
