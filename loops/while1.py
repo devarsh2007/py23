6c6315ef090a5759f282efe183c62db84ae64ae5
devarsh2007 <sunilpatel2131@gmail.com>
2025-03-24 09:52:01 +0530

    loop

---
+++ loops/while1.py
@@ -0,0 +1,17 @@
+# ********** ... n times
+
+n = int(input("enter a number : "))
+symbol = input("enter a symbol : ")
+
+count = 1
+while count<=n:
+    print(f"{symbol}",end=" ")
+    count+=1     #increment
+
+# print("*",end=" ")
+# print("*",end=" ")
+# print("*",end=" ")
+# print("*",end=" ")
+
+
+# print("* "*10)
