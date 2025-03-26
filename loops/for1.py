ac709d1107b51c6845b4babc172f030d1baf99ca
devarsh2007 <sunilpatel2131@gmail.com>
2025-03-26 09:57:00 +0530

    loop

---
+++ loops/for1.py
@@ -0,0 +1,31 @@
+# for loop
+# 1) range - start=0 , stop+1 , size=1
+# 2) sequnce - list,tuple,str,dict....
+
+# write a program that print table for n
+# 5 x 1 = 5
+# ------------------------
+# 5 x 2 = 10
+# ------------------------
+# 5 x 3 = 15
+# ------------------------
+
+
+n = int(input("enter a number : "))
+
+for i in range(1,11,1):
+    ans = n*i
+    print(f"{n} x {i} = {ans}")
+    print("-"*13)
+
+
+# ans = n*count
+# print(f"{n} x {count} = {ans}")
+
+# count+=1
+# ans = n*count
+# print(f"{n} x {count} = {ans}")
+
+# count+=1
+# ans = n*count
+# print(f"{n} x {count} = {ans}")
---
+++ loops/for2.py
@@ -0,0 +1,28 @@
+# write a program that print a number and odd or even
+# 5
+# odd = [1,3,5] even = [2,4]
+
+n = 5
+
+for i in range(1,n+1,1):
+    if i%2==0:
+        print("even")
+        
+    else:
+        print("odd")
+        
+
+# if i%2==0:
+#     print("even")
+    
+# else:
+#     print("odd")
+    
+# i+=1
+# if i%2==0:
+#     print("even")
+    
+# else:
+#     print("odd")
+    
+    
