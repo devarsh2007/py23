f4b74aa03c5ab5ff0b81c9e763a2fc3c8cc9e55f
devarsh2007 <sunilpatel2131@gmail.com>
2025-03-27 10:04:41 +0530

    for

--- loops/for2.py
+++ loops/for2.py
@@ -2,14 +2,21 @@
 # 5
 # odd = [1,3,5] even = [2,4]
 
-n = 5
+n = int(input("enter a number : "))
+even={}
+odd={}
 
 for i in range(1,n+1,1):
     if i%2==0:
-        print("even")
+        # print("even")
+        even[i] = i*i
         
     else:
-        print("odd")
+        # print("odd")
+        odd[i] = i*i
+        
+print("even : ",even)
+print("odd : ",odd)
         
 
 # if i%2==0:
