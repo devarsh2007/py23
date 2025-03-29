d57f7fca790a1ceac60ec99d6edaeb089fdb02b0
devarsh2007 <sunilpatel2131@gmail.com>
2025-03-29 09:57:41 +0530

    function

---
+++ functions/function1.py
@@ -0,0 +1,25 @@
+# print("-----------------------------")
+# print("python")
+# print("-----------------------------")
+
+# define function
+def printline():
+    print("-----------------------------")
+
+def addtion(a,b):
+    print("addition : ",a+b)
+
+# function calling
+printline()
+printline()
+print("python")
+printline()
+printline()
+
+# addtion(4,5)
+
+n1 = int(input("enter number1 : "))
+n2 = int(input("enter number2 : "))
+addtion(n1,n2)
+
+addtion(3,4)
