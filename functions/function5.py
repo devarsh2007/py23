bb66cb589401a99f0c463b5c9c70dab428a618b6
devarsh2007 <sunilpatel2131@gmail.com>
2025-04-08 08:45:30 +0530

    function

--- functions/function5.py
+++ functions/function5.py
@@ -1,7 +1,12 @@
 # write function that calculate sum of n numbers
 
 def getsum(*n):
-    return n
+    sum=0
+    l1=n[0]
+    for i in range(0,len(l1)):
+        sum=sum+l1[i]
+        
+    return sum
 
 l1=[]
 choise = int(input("enter number of input : "))
@@ -10,4 +15,13 @@ for i in range(choise):
     value = int(input("enter value : "))
     l1.append(value)
     
-print(l1)
+# print(l1)
+
+print(getsum(l1))
+
+# l1=[1,2,3]
+# a=0
+# for i in range(0,3):
+#     a=a+l1[i] 
+    
+# print(a)
